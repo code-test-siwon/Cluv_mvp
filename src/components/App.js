2c8bcec4507f231baa57336ec20425a56cf53503
siwon <siwon3376@gmail.com>
2021-01-28 10:04:39 +0900

    global styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 import AppRouter from "components/AppRouter";
-import PropTypes from "prop-types";
+
+import styled from "styled-components";
+import GlobalStyle from "GlobalStyle";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      <GlobalStyle />
+      <WidthLine>
+        <AppRouter isLoggedIn={isLoggedIn} />
+      </WidthLine>
     </>
   );
 };
+
+const WidthLine = styled.div`
+  max-width: 375px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin: auto;
+  border: 1px solid white;
+`;
 
 export default App;
